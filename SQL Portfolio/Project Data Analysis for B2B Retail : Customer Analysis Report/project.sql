e3226c4564165edc4c7f6bf4a0e96dca84b7aab4
RafiWidyansyah <61896896+RafiWidyansyah@users.noreply.github.com>
2025-04-02 20:21:50 +0700

    Update project.sql
    Add some queries

--- SQL Portfolio/Project Data Analysis for B2B Retail : Customer Analysis Report/project.sql
+++ SQL Portfolio/Project Data Analysis for B2B Retail : Customer Analysis Report/project.sql
@@ -1,1 +1,17 @@
+-- Understand Table --
+select * from orders_1 limit 5;
+
+select * from orders_2 limit 5;
+
+select * from customer limit 5;
+
+-- Total Sales and Revenue for Quarter-1 (Jan-Mar) and Quarter-2 (Apr-Jun) --
+## Quarter-1
+SELECT SUM(quantity) AS total_penjualan, SUM(priceEach * quantity) as revenue 
+FROM orders_1 
+WHERE status = "Shipped";
+## Quarter-2
+SELECT SUM(quantity) AS total_penjualan, SUM(priceEach * quantity) as revenue
+FROM orders_2 
+WHERE status = "Shipped";
 
